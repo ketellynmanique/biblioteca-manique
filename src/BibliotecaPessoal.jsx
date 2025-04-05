7c7a2e61d1baa5ae3eb2fbb38efd764cc78072db
ketellynmanique <ketellynmanique@gmail.com>
2025-04-04 23:03:36 -0300

    Update BibliotecaPessoal.jsx

--- src/BibliotecaPessoal.jsx
+++ src/BibliotecaPessoal.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import { Textarea } from "@/components/ui/textarea";
+import { Input } from "./components/ui/input";
+import { Button } from "./components/ui/button";
+import { Card, CardContent } from "./components/ui/card";
+import { Textarea } from "./components/ui/textarea";
 
 export default function BibliotecaPessoal() {
   const [livros, setLivros] = useState([]);
